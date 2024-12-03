7b8dbac9f4ac59711f65d5c443ecba4e2786c5d9
Ruchir Kafle <ruchirkafle@gmail.com>
2024-12-03 11:09:55 -0500

    movement

---
+++ Globals/python_defaults/player.py
@@ -0,0 +1,60 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+
+        self.player_scale = (99, 57)
+
+        self.img = pygame.image.load("globals/assets/character-sprite.png").convert()
+        self.img = pygame.transform.scale(self.img, self.player_scale)
+
+        self.images.append(self.img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 0
+        self.rect.y = 0
+
+        self.acceleration_due_to_gravity = 0.25
+        self.current_gravity = 0
+
+        self.jump_increment = 10
+        self.jump_decrement = 0.25
+        self.current_jump = 0
+        self.apply_jump = False
+
+        self.walk_speed = 5
+
+    def apply_gravity(self, screen_y):
+        if not self.apply_jump:
+            if self.rect.y + self.current_gravity < screen_y - self.player_scale[1]:
+                self.current_gravity += self.acceleration_due_to_gravity
+            else:
+                self.current_gravity = 0
+
+            self.rect.y += self.current_gravity
+
+    def jump(self, screen_y, jumping):
+        if jumping:
+            if self.rect.y + self.current_gravity == screen_y - self.player_scale[1]:
+                self.apply_jump = True
+                self.current_jump = self.jump_increment
+                self.current_gravity = 0
+
+        if self.apply_jump:
+            if self.current_jump >= self.jump_decrement:
+                self.current_jump -= self.jump_decrement
+            else:
+                self.apply_jump = False
+                self.current_jump = 0
+
+        self.rect.y -= self.current_jump
+
+    def walk(self, direction, screen_x):
+        self.rect.x += (direction * self.walk_speed)
+    
+    def run(self, screen_y):
+        self.apply_gravity(screen_y)
+        self.jump(screen_y, False)
--- unit_2/intro_to_boilerplate.py
+++ unit_2/intro_to_boilerplate.py
@@ -1,11 +1,8 @@
 # Importing Pygame.
 import sys
 import pygame
-# from do_not_touch import player
+from globals.python_defaults import player
 from globals.python_defaults import colors
-from globals.python_defaults import fonts
-
-print(colors.WHITE)
 
 # Pygame must be initialized before running.
 pygame.init()
@@ -19,13 +16,18 @@ clock = pygame.time.Clock()
 
 # Creating a window
 # Screen size.
-screen_size = (700, 500) 
+screen_size = (1000, 714) 
 
 # Creating the actual screen.
 screen = pygame.display.set_mode(screen_size)
 
 # Giving the screen a name.
 pygame.display.set_caption("unit-01: Introduction to Boilerplate")
+
+# Player variables, DO NOT TOUCH!
+the_player = player.Player()
+player_list = pygame.sprite.Group()
+player_list.add(the_player)
 
 # Main loop. Everything you want to happen in the game is put in this loop.
 while not done:
@@ -36,16 +38,26 @@ while not done:
         # If the user presses the "X" button in the top right corner, done will be set to true, ending the loop, and thus the game.
         if event.type == pygame.QUIT:
             done = True
+        
+
+    # Getting user input.
+    pressed = pygame.key.get_pressed()
+    
+    if pressed[pygame.K_a]:
+        the_player.walk(-1, screen_size[0])
+
+    if pressed[pygame.K_d]:
+        the_player.walk(1, screen_size[0])
+
+    if pressed[pygame.K_SPACE]:
+        the_player.jump(screen_size[1], True)
 
     # Setting the screen to a black background.
-    screen.fill((0, 0, 0))
+    screen.fill(colors.BLACK)
 
-    # the_player = player.Player()
-
-    # player_list = pygame.sprite.Group()
-    # player_list.add(the_player)
-
-    # player_list.draw(screen)
+    # Player functions, DO NOT TOUCH.
+    the_player.run(screen_size[1])
+    player_list.draw(screen)
 
     # End commands.
     # Rendering the game.
