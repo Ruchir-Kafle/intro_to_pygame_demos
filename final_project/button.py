90ec843842bd83aafbf4e90b2527f371dc9130d9
Ruchir <ruchirkafle@gmail.com>
2024-12-28 19:12:08 -0500

    working on buttons being a sprite

--- final_project/button.py
+++ final_project/button.py
@@ -2,10 +2,14 @@ import pygame
 from globals.python_defaults import colors
 
 class Button(pygame.sprite.Sprite):
-    def __init__(self, screen_size, size_divisor, coordinates):
-        self.screen = pygame.Surface((screen_size["x"] / size_divisor["x"], screen_size["y"] / size_divisor["y"]), pygame.SRCALPHA)
-        self.screen.fill(colors.BLUE)
+    def __init__(self, screen_size, size_divisor, coordinates, group):
+        pygame.sprite.Sprite.__init__(self)
         
-        self.rect = self.screen.get_rect()        
+        self.image = pygame.Surface((screen_size["x"] / size_divisor["x"], screen_size["y"] / size_divisor["y"]), pygame.SRCALPHA)
+        self.image.fill(colors.BLUE)
+        
+        self.rect = self.image.get_rect()        
         self.rect.x = coordinates["x"]
         self.rect.bottom = coordinates["y"]
+
+        group.add(self)
--- final_project/win_screen.py
+++ final_project/win_screen.py
@@ -4,10 +4,15 @@ from final_project import button
 
 class Win_Screen():
     def __init__(self, screen_size):
+        button_group = pygame.sprite.Group()
+        
         color = (*colors.BLACK, 50)
 
         self.screen = pygame.Surface((screen_size["x"], screen_size["y"]), pygame.SRCALPHA)
         self.screen.fill(color)
 
-        a_button = button.Button(screen_size)
-        self.screen.blit(a_button.screen, (500, 500))
+        a_button = button.Button(screen_size, {"x": 500, "y": 500}, {"x": 7, "y": 7}, button_group)
+
+        print(a_button)
+        
+        button_group.draw(self.screen)
