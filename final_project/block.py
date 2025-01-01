43b1511aaed600092dd2414e4f8a857979a5b50c
Ruchir-Kafle <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-01-01 02:04:05 -0500

    textures properly work now

--- final_project/block.py
+++ final_project/block.py
@@ -9,12 +9,30 @@ class Block(pygame.sprite.Sprite):
 
         if self.type == 0:
             self.image = pygame.image.load("final_project/assets/blocks/basic_spike.webp").convert_alpha()
-        elif self.type == 4:
+        elif self.type == 1:
             self.image = pygame.image.load("final_project/assets/blocks/under_grid_block.webp").convert_alpha()
-        elif self.type == 11:
+        elif self.type == 2:
+            self.image = pygame.image.load("final_project/assets/blocks/background_block.webp").convert_alpha()
+        elif self.type == 3:
+            self.image = pygame.image.load("final_project/assets/blocks/GridBlock02.webp").convert_alpha()
+            self.image = pygame.transform.rotate(self.image, 270)
+        elif self.type == 4:
             self.image = pygame.image.load("final_project/assets/blocks/top_grid_block.webp").convert_alpha()
+        elif self.type == 5:
+            self.image = pygame.image.load("final_project/assets/blocks/GridBlock03.webp").convert_alpha()
+        elif self.type == 10:
+            self.image = pygame.image.load("final_project/assets/blocks/GridBlock03.webp").convert_alpha()
+            self.image = pygame.transform.flip(self.image, True, False)
         elif self.type == 13:
             self.image = pygame.image.load("final_project/assets/blocks/basic_block.webp").convert_alpha()
+        elif self.type == 14:
+            self.image = pygame.image.load("final_project/assets/blocks/GridBlock02.webp").convert_alpha()
+        elif self.type == 15:
+            self.image = pygame.image.load("final_project/assets/blocks/GridBlock02.webp").convert_alpha()
+            self.image = pygame.transform.rotate(self.image, 90)
+        else:
+            self.image = pygame.image.load("final_project/assets/blocks/basic_thorns.webp").convert_alpha()
+
 
         self.image = pygame.transform.scale(self.image, size)
 
